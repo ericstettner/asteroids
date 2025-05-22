03f4f126fd6e0710e323355fe4f48a6d95b6a244
ericstettner <estettner4@gmail.com>
2025-05-22 13:35:05 -0400

    add fps to game

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from constants import *
 def main():
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+  clock = pygame.time.Clock()
+  dt = 0
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -15,6 +18,7 @@ def main():
       
     screen.fill(000)
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
